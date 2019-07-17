d3727b18d8e3346607ef8b84102c50596a128748
liukai <liukai@yzyw-inc.com>
2019-07-17 19:47:01 +0800

    MOD: 使用 log4j2

--- feign-consumer/src/main/java/com/look/feignconsumer/ConsumerApplication.java
+++ feign-consumer/src/main/java/com/look/feignconsumer/ConsumerApplication.java
@@ -1,6 +1,7 @@
 package com.look.feignconsumer;
 
 import com.look.feignconsumer.service.HelloServiceFallback;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,10 +12,13 @@ import org.springframework.context.annotation.Bean;
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@Slf4j
 public class ConsumerApplication {
 
   public static void main(String[] args) {
+    log.debug("[start application]\t[1]");
     SpringApplication.run(ConsumerApplication.class, args);
+    log.debug("[start application]\t[2]");
   }
 
 }
